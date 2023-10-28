6ea8c0599e6c25ea4403ba11549bbb7ed7ba3eba
Djani5 <75142723+Djani5@users.noreply.github.com>
2023-10-28 14:11:59 +0200

    Add files via upload

--- sketch_oct28b.ino
+++ sketch_oct28b.ino
@@ -1,22 +1,32 @@
 #include <Servo.h>
-
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-
+const int trigPin = 9;
+const int echoPin = 10;
+Servo myservo;  
+float duration;
+float ugao = 0;
 int pos = 0;    
-
+int mode = 1;
 void setup() {
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  Serial.begin(9600); 
   myservo.attach(11);  
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { 
 
-    myservo.write(pos);              
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+
+    ugao += mode * duration/200;
+    if(ugao >= 180) mode = -1;
+    if(ugao <= 1) mode = 1;
+    myservo.write(ugao);              
     delay(15);                      
-  }
-  for (pos = 180; pos >= 0; pos -= 1) {
-    myservo.write(pos);             
-    delay(15);                       
-  }
+          
+  
 }
